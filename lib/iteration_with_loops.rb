ad67861522c203bbc078ddd54bd12907fad2e10c
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-17 23:27:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,8 +3,8 @@ def find_min_in_nested_arrays(src)
   array_of_daily_temp = []
   
     while row_index < src.count do 
-      element_index = 0
-      min_integer_element = 10000
+      element_row_index = 0
+      element_collumn_index = 10000
         while element_index < src[row_index].count do 
           if src[row_index][element_index] < min_integer_element
             min_integer_element = src[row_index][element_index]
